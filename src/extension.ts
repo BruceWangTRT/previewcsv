ccd08ff8f4cf08dc567a4b3070850b77e15feab9
yanxin wang <wangyanxin89@gmail.com>
2018-07-23 17:10:30 -0400

    Open unformatted csv preview in new window
    When previewing, show the first 4 lines of a .csv file in a new tab.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as readline from 'readline';
+const MAXLINE = 4;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,11 +17,30 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.sayHello', (uri) => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
+        let i = 0;
+        let previewContent = '';
+        let csvStream = fs.createReadStream(uri.path);
+        let rl = readline.createInterface(csvStream);
+        rl.on('line', (line) => {
+            if(i>= MAXLINE) 
+            { 
+                rl.close(); 
+                return;
+            }
+            previewContent = previewContent.concat(line, '\n');
+            console.log(line);
+            i++;
+        });
+        rl.on('close', ()=> {
+            csvStream.close();
+            vscode.workspace.openTextDocument({content: previewContent})
+            .then(doc => vscode.window.showTextDocument(doc));
+        });
     });
 
     context.subscriptions.push(disposable);
