333e77db4f3be0e2d54cc931fc30775ba256b97c
yanxin wang <wangyanxin89@gmail.com>
2018-07-25 15:50:21 -0400

    Clean up
    Clean up code. Update README with gif for better demo. Remove useless
    dependencies as they are built-in modules.

--- src/extension.ts
+++ src/extension.ts
@@ -4,15 +4,11 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as readline from 'readline';
-const MAXLINE = 4;
+const DEFAULT_LINES_TO_PREVIEW = 4;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "previewcsv" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
@@ -20,25 +16,34 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.previewCsv', (uri) => {
         // The code you place here will be executed every time your command is executed
 
+        // Get number of lines to preview from configuration.
+        // Fallback to DEFAULT_LINES_TO_PREVIEW in case of failure.
+        let linesToPreview = vscode.workspace.getConfiguration("previewcsv").get("linesToPreview")
+                            || DEFAULT_LINES_TO_PREVIEW;
+
         // Display a message box to the user
-        vscode.window.showInformationMessage('Previewing first y lines of xxxxx.csv');
+        vscode.window.showInformationMessage(`Previewing first ${linesToPreview} lines of ${uri.path}`);
+
+        // Open the selected file and read line by line
         let i = 0;
         let previewContent = '';
         let csvStream = fs.createReadStream(uri.path);
         let rl = readline.createInterface(csvStream);
         rl.on('line', (line) => {
-            if(i>= MAXLINE) 
+            if(i >= linesToPreview) 
             { 
                 rl.close(); 
                 return;
             }
             previewContent = previewContent.concat(line, '\n');
             i++;
-        });
-        rl.on('close', ()=> {
+        }).on('close', ()=> {
             csvStream.close();
+            // remove the last '\n'
+            previewContent = previewContent.slice(0, -1);
+            // Prepare a TextDocument and show it in a new tab
             vscode.workspace.openTextDocument({language: 'CSV', content: previewContent})
-                .then(doc => vscode.window.showTextDocument(doc));
+                .then(doc => vscode.window.showTextDocument(doc, {preview: false}));
         });
     });
 
